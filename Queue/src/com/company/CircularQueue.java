454d37bdb5159bcde7c7267d6dad7b12fde67f00
Reza Mousavian <rezamosavian1014@gmail.com>
2020-12-05 07:41:29 -0800

    add circularQueue

---
+++ Queue/src/com/company/CircularQueue.java
@@ -0,0 +1,65 @@
+package com.company;
+
+public class CircularQueue {
+    private int capacity;
+    private int[] circularQueue;
+    private int rear, front, size;
+
+    public CircularQueue(int capacity) {
+        this.capacity = capacity;
+        this.circularQueue = new int[capacity];
+        front = this.size = 0;
+        this.rear = -1;
+    }
+
+    public CircularQueue(int capacity, int[] circularQueue) {
+        this.capacity = capacity;
+        this.circularQueue = circularQueue;
+        this.front = 0;
+        this.rear = -1;
+    }
+
+    public int getRear() {
+        return rear;
+    }
+
+    public int getFront() {
+        return front;
+    }
+
+    private boolean isEmpty() {
+        return (this.size == 0);
+    }
+
+    private boolean isFull() {
+        return (this.circularQueue.length == this.size);
+    }
+
+    public boolean enqueue(int data) {
+        if (this.isFull()) {
+            System.out.println("Queue is full");
+            return false;
+        }
+        this.rear = (this.rear + 1)
+                % this.capacity;
+        this.circularQueue[this.rear] = data;
+        this.size = this.size + 1;
+        System.out.println(data
+                + " enqueued to queue");
+        return true;
+    }
+
+    public int dequeue() {
+        if (this.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        int item = this.circularQueue[this.front];
+        this.front = (this.front + 1)
+                % this.capacity;
+        System.out.println(item + "dequeue from Queue");
+        this.size = this.size - 1;
+        return item;
+    }
+
+}
--- Queue/src/com/company/Main.java
+++ Queue/src/com/company/Main.java
@@ -4,24 +4,42 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Queue queue = new Queue(10);
-        queue.enqueue(10);
-        queue.enqueue(2);
-        queue.enqueue(120);
-        queue.enqueue(100);
-        queue.enqueue(105);
-        System.out.println(queue);
-        System.out.println(queue.peek());
-        queue.dequeue();
-        queue.dequeue();
-        System.out.println(queue.peek());
-        System.out.println(queue);
-        queue.enqueue(10000);
-        queue.enqueue(10000);
-        System.out.println(queue);
-        queue.enqueue(20000);
-        queue.enqueue(30000);
-        queue.enqueue(40000);
-        System.out.println(queue);
+//        Queue queue = new Queue(10);
+//        queue.enqueue(10);
+//        queue.enqueue(2);
+//        queue.enqueue(120);
+//        queue.enqueue(100);
+//        queue.enqueue(105);
+//        System.out.println(queue);
+//        System.out.println(queue.peek());
+//        queue.dequeue();
+//        queue.dequeue();
+//        System.out.println(queue.peek());
+//        System.out.println(queue);
+//        queue.enqueue(10000);
+//        queue.enqueue(10000);
+//        System.out.println(queue);
+//        queue.enqueue(20000);
+//        queue.enqueue(30000);
+//        queue.enqueue(40000);
+//        System.out.println(queue);
+        CircularQueue circularQueue = new CircularQueue(5);
+
+        circularQueue.enqueue(10);
+        circularQueue.enqueue(20);
+        circularQueue.enqueue(30);
+        circularQueue.enqueue(40);
+        circularQueue.enqueue(50);
+        circularQueue.dequeue();
+        circularQueue.enqueue(60);
+
+//        System.out.println(circularQueue.dequeue()
+//                + " dequeued from queue\n");
+//
+//        System.out.println("Front item is "
+//                + circularQueue.getFront());
+//
+//        System.out.println("Rear item is "
+//                + circularQueue.getRear());
     }
 }
